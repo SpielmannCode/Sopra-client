4f7d99adeae46a48d6006a48f99132ba1dae2fe2
Valentin Weiss <valentin.weiss@hotmail.com>
2017-03-21 18:17:39 +0100

    creating games with name from form works

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import {HttpModule, JsonpModule} from '@angular/http';
 
 import { AppComponent } from './app.component';
@@ -24,6 +24,7 @@ import { InfoComponent } from './info/info.component';
   imports: [
     BrowserModule,
     FormsModule,
+    ReactiveFormsModule,
     HttpModule,
     JsonpModule,
     routing
--- src/app/lobby/lobby.component.ts
+++ src/app/lobby/lobby.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import {Game} from "../shared/models/game";
 import {UserService} from "../shared/services/user.service";
 import {User} from "../shared/models/user";
+import { FormGroup, FormControl, Validators, FormBuilder }  from '@angular/forms';
+import {Http, RequestOptions, Headers, Response} from "@angular/http";
 
 // Backend noch nix funktionere, darum ich habe gemacht diese
 const GAMES: Game[] = [
@@ -19,8 +21,15 @@ export class LobbyComponent implements OnInit {
   users: User[] = [];
   games: Game[] = GAMES;
   selectedGame: Game;
+  createGameForm: FormGroup;
 
-  constructor(private userService: UserService) { }
+  constructor(private userService: UserService,
+              private fb: FormBuilder,
+              private http: Http) {
+    this.createGameForm = fb.group({
+      name: ["", Validators.required]
+    });
+  }
 
   ngOnInit() {
     // get users from secure api end point
@@ -32,5 +41,15 @@ export class LobbyComponent implements OnInit {
 
   selectGame(game: Game) {
     this.selectedGame = game;
+  }
+
+  createGame() {
+    let headers = new Headers({ 'Content-Type': 'application/json' });
+    let options = new RequestOptions({ headers: headers });
+    let userToken = JSON.parse(localStorage.getItem('currentUser')).token;
+
+
+    return this.http.post('http://localhost:8080/games?token=' + userToken,
+      JSON.stringify(this.createGameForm.value), options).subscribe();
   }
 }
