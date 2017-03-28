77380dd1cee4fa6597e08fd419413bf9c2706ba6
Valentin Weiss <valentin.weiss@hotmail.com>
2017-03-28 20:00:10 +0200

    pass game object to playingfield components

--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import {UserService} from "../shared/services/user.service";
 import {User} from "../shared/models/user";
+import {ActivatedRoute} from "@angular/router";
+import {GameService} from "../shared/services/game.service";
+import {Game} from "../shared/models/game";
 
 @Component({
   selector: 'app-game',
@@ -8,9 +11,13 @@ import {User} from "../shared/models/user";
   styleUrls: ['./game.component.css']
 })
 export class GameComponent  implements OnInit {
-  users: User[] = [];
+  protected users: User[] = [];
+  protected gameId: number;
+  protected game: Game;
 
-  constructor(private userService: UserService) { }
+  constructor(protected userService: UserService,
+              protected gameService: GameService,
+              protected route: ActivatedRoute) { }
 
   ngOnInit() {
     // get users from secure api end point
@@ -18,6 +25,15 @@ export class GameComponent  implements OnInit {
       .subscribe(users => {
         this.users = users;
       });
-  }
 
+    this.route.params.subscribe(params => {
+      this.gameId = params['id'];
+      console.log(this.gameId);
+
+      this.gameService.getGame(this.gameId).subscribe(game => {
+        this.game = game;
+        console.log(game);
+      });
+    });
+  }
 }
