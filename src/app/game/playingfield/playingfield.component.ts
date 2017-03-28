fc8cdcc3eaa95b10323e83c1382f1cee5180f0c8
Valentin Weiss <valentin.weiss@hotmail.com>
2017-03-28 20:00:39 +0200

    work on statsboard

--- src/app/game/playingfield/playingfield.component.ts
+++ src/app/game/playingfield/playingfield.component.ts
@@ -1,6 +1,4 @@
-import { Component, OnInit } from '@angular/core';
-import {ActivatedRoute} from "@angular/router";
-import {GameService} from "../../shared/services/game.service";
+import {Component, Input, OnInit} from '@angular/core';
 import {Game} from "../../shared/models/game";
 
 @Component({
@@ -9,23 +7,12 @@ import {Game} from "../../shared/models/game";
   styleUrls: ['./playingfield.component.css']
 })
 export class PlayingfieldComponent implements OnInit {
-  gameId: number;
-  game: Game;
 
-  constructor(protected gameService: GameService,
-              protected route: ActivatedRoute) { }
+  @Input('game') game: Game;
+
+  constructor() {
+  }
 
   ngOnInit() {
-    this.route.params.subscribe(params => {
-      this.gameId = params['id'];
-      console.log(this.gameId);
-
-      this.gameService.getGame(this.gameId).subscribe(game => {
-        this.game = game;
-        console.log(game);
-      });
-    });
-
-
   }
 }
