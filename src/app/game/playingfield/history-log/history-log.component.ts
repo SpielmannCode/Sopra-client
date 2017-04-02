1280a070649c0f28f30fb4a03ec22b0fc34cd651
SpielmannCode <nicolas.spielmann@outlook.de>
2017-04-02 16:26:02 +0200

    Trying to add the history log

--- src/app/game/playingfield/history-log/history-log.component.ts
+++ src/app/game/playingfield/history-log/history-log.component.ts
@@ -1,15 +1,28 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input, OnChanges, OnInit} from '@angular/core';
+import {Move} from '../../../shared/models/move';
+import {Game} from '../../../shared/models/game';
+import {MoveService} from '../../../shared/services/move.service';
 
 @Component({
   selector: 'app-history-log',
   templateUrl: './history-log.component.html',
   styleUrls: ['./history-log.component.css']
 })
-export class HistoryLogComponent implements OnInit {
+export class HistoryLogComponent implements OnInit, OnChanges {
+  protected moves: Move[] = [];
+  @Input('game') game: Game;
 
-  constructor() { }
+  constructor(protected moveservice: MoveService) { }
 
   ngOnInit() {
+      this.moveservice.getMoves(1).subscribe(moves => {
+        this.moves = moves;
+      });
+  }
+  ngOnChanges(){
+    this.moveservice.getMoves(1).subscribe(moves => {
+      this.moves = moves;
+    });
   }
 
 }
