f096408843b53d741be233223a9ec97eaa47c473
Valentin Weiss <valentin.weiss@hotmail.com>
2017-04-04 15:21:43 +0200

    player cardstack now shows cards from backend

--- src/app/game/statsboard/cardstack/cardstack.component.ts
+++ src/app/game/statsboard/cardstack/cardstack.component.ts
@@ -1,15 +1,32 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input, OnChanges, OnInit} from '@angular/core';
+import {Game} from "../../../shared/models/game";
 
 @Component({
   selector: 'app-cardstack',
   templateUrl: './cardstack.component.html',
   styleUrls: ['./cardstack.component.css']
 })
-export class CardstackComponent implements OnInit {
+export class CardstackComponent implements OnInit, OnChanges {
+  @Input('game') game: Game;
+  playerCards;
 
   constructor() { }
 
   ngOnInit() {
+    this.setPlayerCards();
+  }
+
+  ngOnChanges() {
+    this.setPlayerCards();
+  }
+
+  setPlayerCards() {
+    let userToken = JSON.parse(localStorage.getItem('currentUser')).token;
+    for (let player of this.game.players) {
+      if (player.token === userToken) {
+        this.playerCards = player.cards;
+      }
+    }
   }
 
 }
