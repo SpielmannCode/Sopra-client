2b6bf2dd910dca9635d504f1732c9377423d3c09
pizzarafi <rafael.sajtschik@gmail.com>
2017-04-20 18:54:03 +0200

    pyramidLevel1 background

--- src/app/game/playingfield/site/pyramid/pyramid.component.ts
+++ src/app/game/playingfield/site/pyramid/pyramid.component.ts
@@ -8,8 +8,11 @@ import {Component, Input, OnInit} from '@angular/core';
 export class PyramidComponent implements OnInit {
   @Input('pyramidSite') pyramidSite;
   @Input('dockOpen') dockOpen;
+  fields;
+  divArrayLv1 : number[] = [2, 1, 3,2,4,3,2,1,3];
 
   constructor() {
+
   }
 
   ngOnInit() {
