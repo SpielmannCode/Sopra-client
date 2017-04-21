e03e0d4d3acd729c2224083b9637c471bcc8afd6
Valentin Weiss <valentin.weiss@hotmail.com>
2017-04-21 21:05:27 +0200

    water canvas with perlin noise works

--- src/assets/js/watercanvas.js
+++ src/assets/js/watercanvas.js
@@ -1,20 +1,24 @@
-var PerlinNoise = new function() {
+// src: http://www.html5gamedevelopment.org/html5-game-tutorials/2012-01-procedural-textures-in-html5-canvas
+// This is a port of Ken Perlin's Java code. The
+// original Java code is at http://cs.nyu.edu/%7Eperlin/noise/.
+// Note that in this version, a number from 0 to 1 is returned.
+PerlinNoise = new function() {
 
   this.noise = function(x, y, z) {
 
     var p = new Array(512)
-    var permutation = [ 151,160,137,91,90,15,
+    var permutation = [151,160,137,91,90,15,
       131,13,201,95,96,53,194,233,7,225,140,36,103,30,69,142,8,99,37,240,21,10,23,
       190, 6,148,247,120,234,75,0,26,197,62,94,252,219,203,117,35,11,32,57,177,33,
       88,237,149,56,87,174,20,125,136,171,168, 68,175,74,165,71,134,139,48,27,166,
       77,146,158,231,83,111,229,122,60,211,133,230,220,105,92,41,55,46,245,40,244,
-      102,143,54, 65,25,63,161, 1,216,80,73,209,76,132,187,208, 89,18,169,200,196,
-      135,130,116,188,159,86,164,100,109,198,173,186, 3,64,52,217,226,250,124,123,
+      102,143,54, 65,25,63,161,1,216,80,73,209,76,132,187,208,89,18,169,200,196,
+      135,130,116,188,159,86,164,100,109,198,173,186,3,64,52,217,226,250,124,123,
       5,202,38,147,118,126,255,82,85,212,207,206,59,227,47,16,58,17,182,189,28,42,
-      223,183,170,213,119,248,152, 2,44,154,163, 70,221,153,101,155,167, 43,172,9,
-      129,22,39,253, 19,98,108,110,79,113,224,232,178,185, 112,104,218,246,97,228,
-      251,34,242,193,238,210,144,12,191,179,162,241, 81,51,145,235,249,14,239,107,
-      49,192,214, 31,181,199,106,157,184, 84,204,176,115,121,50,45,127, 4,150,254,
+      223,183,170,213,119,248,152,2,44,154,163,70,221,153,101,155,167,43,172,9,
+      129,22,39,253,19,98,108,110,79,113,224,232,178,185,112,104,218,246,97,228,
+      251,34,242,193,238,210,144,12,191,179,162,241,81,51,145,235,249,14,239,107,
+      49,192,214,31,181,199,106,157,184,84,204,176,115,121,50,45,127,4,150,254,
       138,236,205,93,222,114,67,29,24,72,243,141,128,195,78,66,215,61,156,180
     ];
     for (var i=0; i < 256 ; i++)
@@ -40,7 +44,7 @@ var PerlinNoise = new function() {
         grad(p[BA+1], x-1, y  , z-1 )), // OF CUBE
         lerp(u, grad(p[AB+1], x  , y-1, z-1 ),
           grad(p[BB+1], x-1, y-1, z-1 )))));
-  };
+  }
   function fade(t) { return t * t * t * (t * (t * 6 - 15) + 10); }
   function lerp( t, a, b) { return a + t * (b - a); }
   function grad(hash, x, y, z) {
@@ -50,9 +54,52 @@ var PerlinNoise = new function() {
     return ((h&1) == 0 ? u : -u) + ((h&2) == 0 ? v : -v);
   }
   function scale(n) { return (1 + n)/2; }
+}
+
+
+// Beispiel:
+var canvas = document.getElementById("noiseCanvas");
+
+var shader = function(r, g, b, a, x, y, w, h) {
+  x /= w;
+  y /= h; // normalize
+  var size = 10;  // pick a scaling value
+  var n = PerlinNoise.noise(size * x, size * y, 0.8);
+  r = g = b = Math.round(255 * n);
+
+  return {r:r, g:g, b:b, a:255};
 };
 
-function WaterCanvas() {
+var manipulateImageData = function(canvas, shader) {
+  var w = canvas.width;
+  var h = canvas.height;
+  var context  = canvas.getContext("2d");
+
+  var imageData = context.createImageData(w, h);
+
+  for (var i = 0, l = imageData.data.length; i < l; i += 4) {
+    var x = (i / 4) % w;
+    var y = Math.floor(i / w / 4);
+
+    var r = imageData.data[i + 0];
+    var g = imageData.data[i + 1];
+    var b = imageData.data[i + 2];
+    var a = imageData.data[i + 3];
+
+    var pixel = shader(r, g, b, a, x, y, w, h);
+
+    imageData.data[i  ] = pixel.r;
+    imageData.data[i + 1] = pixel.g;
+    imageData.data[i + 2] = pixel.b;
+    imageData.data[i + 3] = pixel.a;
+  }
+
+  context.putImageData(imageData, 0, 0);
+};
+
+
+
+function WaterCanvas(img) {
   var canvas = document.getElementById('water-background'),
     /** @type {CanvasRenderingContext2D} */
     ctx = canvas.getContext('2d'),
@@ -81,27 +128,21 @@ function WaterCanvas() {
    * (see example here: http://media.chikuyonok.ru/ripple/)
    * But I need to draw simple artwork to bypass 1k limitation
    */
-  var img = document.getElementById('water-img');
 
-  ctx.drawImage(img, 0, 0);
+  // with (ctx) {
+  //   fillStyle = '#fff';
+  //   fillRect(0, 0, width, height);
+  //
+  //   fillStyle = '#0077bb';
+  //   save();
+  //   rotate(-0.785);
+  //   for (var i = 0; i < count; i++) {
+  //     fillRect(-width, i * step, width * 3, line_width);
+  //   }
+  //   restore();
+  // }
 
-  canvas.style.left = img.offsetLeft + 'px';
-  canvas.style.top = img.offsetTop + 'px';
-
-  img.parentNode.insertBefore(canvas, img);
-
-  /*with (ctx) {
-    fillStyle = '#a2ddf8';
-    fillRect(0, 0, width, height);
-
-    fillStyle = '#0077bb';
-    save();
-    rotate(-0.785);
-    for (var i = 0; i < count; i++) {
-      fillRect(-width, i * step, width * 3, line_width);
-    }
-    restore();
-  }*/
+  manipulateImageData(canvas, shader);
 
   texture = ctx.getImageData(0, 0, width, height);
   ripple = ctx.getImageData(0, 0, width, height);
