9d8c905784da69af5289c0400cb54f6c6f10f798
Valentin Weiss <valentin.weiss@hotmail.com>
2017-04-25 10:16:58 +0200

    implement fast forward POST

--- src/app/shared/services/game.service.ts
+++ src/app/shared/services/game.service.ts
@@ -49,4 +49,8 @@ export class GameService {
     return this.http.delete(this.apiService.apiUrl + '/games/' + game.id + '/players?token=' + currentUserToken);
   }
 
+  fastForward(game: Game): Observable<Response> {
+    return this.http.post(this.apiService.apiUrl + '/games/' + game.id + '/fastForward', null, this.apiService.options);
+  }
+
 }
