bf4adf1ff37903b93df2e2d53c9e0349bc9151d4
Valentin Weiss <valentin.weiss@hotmail.com>
2017-04-30 00:01:57 +0200

    implement transitions for all sites

--- src/app/game/playingfield/ship/ship.component.ts
+++ src/app/game/playingfield/ship/ship.component.ts
@@ -1,4 +1,6 @@
 import {animate, Component, Input, OnInit, state, style, transition, trigger} from '@angular/core';
+import {Observable} from "rxjs";
+import {MoveService} from "../../../shared/services/move.service";
 
 @Component({
   selector: 'app-ship',
@@ -9,16 +11,35 @@ import {animate, Component, Input, OnInit, state, style, transition, trigger} fr
       state('onShip', style({
         transform: 'translateX(0)'
       })),
-      state('onSiteLeft', style({
-        transform: 'translateX(-300px)'
+      state('onSitePyramid', style({
+        transform: 'translateX(-390px)'
       })),
       state('onSiteRight', style({
         transform: 'translateX(300px)'
       })),
-      transition('onShip => onSiteLeft', animate('500ms')),
-      transition('onSiteLeft => onShip', animate('500ms')),
-      transition('onShip => onSiteRight', animate('500ms')),
-      transition('onSiteRight => onShip', animate('500ms'))
+      state('onSiteTemple', style({
+        transform: 'translateX(340px)'
+      })),
+      state('onSiteBurialChamber', style({
+        transform: 'translateX(190px)'
+      })),
+      state('onSiteObelisk', style({
+        transform: 'translateX(250px)'
+      })),
+      transition('onShip => onSitePyramid', animate('500ms ease-in')),
+      transition('onSitePyramid => onShip', animate('500ms')),
+
+      transition('onShip => onSiteRight', animate('500ms ease-in')),
+      transition('onSiteRight => onShip', animate('500ms')),
+
+      transition('onShip => onSiteTemple', animate('500ms ease-in')),
+      transition('onSiteTemple => onShip', animate('500ms')),
+
+      transition('onShip => onSiteBurialChamber', animate('500ms ease-in')),
+      transition('onSiteBurialChamber => onShip', animate('500ms')),
+
+      transition('onShip => onSiteObelisk', animate('500ms ease-in')),
+      transition('onSiteObelisk => onShip', animate('500ms'))
     ])
   ]
 })
@@ -33,22 +54,14 @@ export class ShipComponent implements OnInit {
   @Input('reordering') reordering;
   picloc: string = ('/assets/Images/Ship/Ship_' + this.size + 'er.png');
 
-  @Input('shipStoneState') shipStoneState;
+  shipStoneState = 'onShip';
 
-
-constructor() { }
+constructor(private moveService: MoveService) { }
 
   ngOnInit() {
     this.size = this.stones.length;
     this.picloc = ('/assets/Images/Ship/Ship_' + this.size + 'er.png');
   }
 
-  toggleShipStoneState() {
-    if (this.shipStoneState === 'onShip') {
-      this.shipStoneState = 'onSiteRight';
-    } else {
-      this.shipStoneState = 'onShip';
-    }
-  }
 
 }
